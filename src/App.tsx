e84db79805bf195d94a3f67fde00475f8a22a889
Adam Crockett <arc1224@gmail.com>
2023-02-28 16:35:58 -0800

    Create basic component for list of sets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SetList from './Components/SetList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SetList />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/Components/SetList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SetList() {
+  return (
+    <div>
+      <ul>
+        <li> List items here</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SetList;
