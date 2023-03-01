a9c59c1795c59201f7d707034662c0ef6594a570
Adam Crockett <arc1224@gmail.com>
2023-02-28 17:43:16 -0800

    created action type constants

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,9 @@
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const FETCH_SET = 'FETCH_SET';
+export const FETCH_SET_CMDS = 'FETCH_SET_CMDS';
+
+export const START_LOADING = 'START_LOADING';
+export const END_LOADING = 'END_LOADING';
