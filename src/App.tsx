cb28c18346ee3d08004e7fd28aab0abedafdbc0c
Adam Crockett <arc1224@gmail.com>
2023-02-28 17:46:30 -0800

    path rename update on component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SetList from './Components/SetList';
+import SetList from './components/SetList';
 
 function App() {
   return (
