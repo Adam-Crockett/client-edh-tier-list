dcc66f78026ff46f0f34bd2e1ec2d7fac752c779
Adam Crockett <arc1224@gmail.com>
2023-02-28 17:46:07 -0800

    SetList Update for test

--- src/Components/SetList.tsx
+++ src/Components/SetList.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function SetList() {
-  return (
+  // Resolve state typing for typescript
+  const { sets, isLoading } = useSelector((state: any) => state.sets);
+
+  if (!sets.length && !isLoading) {
+    return <div>'No Sets found';</div>;
+  }
+  return isLoading ? (
     <div>
-      <ul>
-        <li> List items here</li>
-      </ul>
+      <p>Loading Sets...</p>
     </div>
+  ) : (
+    <div>{sets}</div>
   );
 }
 
