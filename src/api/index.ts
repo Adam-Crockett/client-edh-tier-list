69c00ccf9b9d73856c54d976c13c7e636265e049
Adam Crockett <arc1224@gmail.com>
2023-02-28 17:45:41 -0800

    axios api request setup for fetchAllSets

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+
+API.interceptors.request.use((req) => {
+  return req;
+});
+
+export const fetchAllSets = () => API.get(`/sets`);
