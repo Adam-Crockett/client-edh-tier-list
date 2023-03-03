6b4e0c48a314aa68d56d4443969e65b9e3fd0889
Adam Crockett <arc1224@gmail.com>
2023-03-02 22:12:13 -0800

    created useGetSets custom hook

---
+++ src/customHooks/useGetSets.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type SetData = {
+  _id: string;
+  createdAt: string;
+  sets: any;
+};
+
+export default function useGetSets() {
+  const [sets, updateSets] = useState<SetData | null>(null);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/sets`).then(({ data }) => {
+      updateSets(data);
+    });
+  }, []);
+  return sets;
+}
