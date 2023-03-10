ed060de13b9597e10b063901010f18a32c86ca95
Adam Crockett <arc1224@gmail.com>
2023-03-09 22:29:34 -0800

    Removed type for data return for test

--- src/customHooks/useGetSets.tsx
+++ src/customHooks/useGetSets.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-type SetData = {
-  _id: string;
-  createdAt: string;
-  sets: any;
-};
+// type SetData = {
+//   _id: string;
+//   createdAt: string;
+//   sets: any;
+// };
 
 export default function useGetSets() {
-  const [sets, updateSets] = useState<SetData | null>(null);
+  const [sets, updateSets] = useState(null);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/sets`).then(({ data }) => {
