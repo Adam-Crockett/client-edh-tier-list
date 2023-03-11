f4cc45e9e1a52a8e01f266a7526ef1da98b98aec
Adam Crockett <arc1224@gmail.com>
2023-03-11 14:01:45 -0800

    Refactored multiselect, removed state, interface

--- src/Components/MultiselectSets.tsx
+++ src/Components/MultiselectSets.tsx
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
-
-function MultiselectSets() {
-  return <></>;
+interface MultiselectSetsProps {
+  options: { id: number; src: string; name: string; code: string }[];
+  selectedIds: number[];
+  onChange: (selectedIds: number[]) => void;
 }
+
+const MultiselectSets = ({
+  options,
+  selectedIds,
+  onChange
+}: MultiselectSetsProps) => {
+  const handleOptionClick = (id: number) => {
+    const newSelectedIds = selectedIds.includes(id)
+      ? selectedIds.filter((selectedId) => selectedId !== id)
+      : [...selectedIds, id];
+    onChange(newSelectedIds);
+  };
+  return (
+    <div>
+      {options.map((option) => (
+        <img
+          key={option.id}
+          src={option.src}
+          alt={option.name}
+          title={option.name}
+          onClick={() => handleOptionClick(option.id)}
+          style={{
+            width: 50,
+            height: 50,
+            opacity: selectedIds.includes(option.id) ? 0.5 : 1
+          }}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default MultiselectSets;
