b52d09076c83e8572f3ac62e75f420a064492f44
Adam Crockett <arc1224@gmail.com>
2023-03-12 22:47:45 -0700

    created custom hook for retreiving card data

---
+++ src/customHooks/useGetCards.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { SelectedSets, CardData } from '../interfaces';
+
+export default function useGetCards({ selectedCodes }: SelectedSets) {
+  const [currentCards, setCurrentCards] = useState<CardData[]>([]);
+  const [loadingCards, setLoading] = useState(true);
+  const [cardError, setCardError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(selectedCodes);
+      try {
+        axios
+          .get(`${process.env.REACT_APP_API_URL}/cards`, {
+            params: { setCodes: selectedCodes }
+          })
+          .then((fetchedData: any) => {
+            setCurrentCards(fetchedData);
+          });
+      } catch (e) {
+        setCardError('Error fetching data');
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [selectedCodes]);
+  return { currentCards, loadingCards, cardError };
+}
