069d221192d78a3680ee6164eebfb288c6270c4d
Adam Crockett <arc1224@gmail.com>
2023-03-17 16:20:25 -0700

    Minor App clean up

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import useGetSets from './customHooks/useGetSets';
 import './App.css';
 import SetList from './components/SetList';
 import CardList from './components/CardList';
-import { SelectedSets } from './interfaces';
 import useGetCards from './customHooks/useGetCards';
 
 const SetContext = createContext('');
 
 function App() {
   const { data, loading, error } = useGetSets();
-  // Implement a solution to deal with removing a set from selectedCodes and updating currentCards
   const [selectedCodes, setSelectedCodes] = useState<string[]>([]);
   const { currentCards, loadingCards, cardError } = useGetCards(selectedCodes);
   const handleMultiselectChange = (newSelectedCodes: string[]) => {
@@ -24,27 +22,21 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-        <div>
-          <CardList currentCards={currentCards} loadingCards={loadingCards} />
-          {loading ? (
-            <></>
-          ) : (
-            <SetList
-              sets={data}
-              selectedCodes={selectedCodes}
-              onMultiselectChange={handleMultiselectChange}
-            />
-          )}
-        </div>
+        <main>
+          <div>
+            <CardList currentCards={currentCards} loadingCards={loadingCards} />
+            {loading ? (
+              <></>
+            ) : (
+              <SetList
+                sets={data}
+                selectedCodes={selectedCodes}
+                onMultiselectChange={handleMultiselectChange}
+              />
+            )}
+          </div>
+        </main>
       </div>
     </SetContext.Provider>
   );
