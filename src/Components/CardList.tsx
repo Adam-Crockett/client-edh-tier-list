2cf4bbdec493bdb84aec9c595959b4ecca47b59c
Adam Crockett <arc1224@gmail.com>
2023-03-17 16:56:02 -0700

    Added image swap for MDFCs

--- src/Components/CardList.tsx
+++ src/Components/CardList.tsx
@@ -1,13 +1,37 @@
+import { CardData } from '../interfaces';
 const CardList = ({ currentCards, loadingCards }: any) => {
-  console.log(currentCards);
+  const handleClickOnMDFC = (
+    event: React.MouseEvent<HTMLImageElement>,
+    card: any
+  ) => {
+    const element = event.target as HTMLImageElement;
+
+    const swapFace: any =
+      element.src == card.card_faces[0].image_uris.small
+        ? card.card_faces[1].image_uris.small
+        : card.card_faces[0].image_uris.small;
+    element.src = swapFace;
+  };
+
   return (
     <ul>
       {loadingCards ? (
         <></>
       ) : (
-        currentCards.map((card: any) => (
-          <img key={card.id} src={card.image_uris.small} />
-        ))
+        currentCards.map((card: any) => {
+          if (card.image_uris) {
+            return <img key={card.id} src={card.image_uris.small} />;
+          } else {
+            return (
+              // <img key={card.id} src={card.image_faces} />
+              <img
+                key={card.id}
+                src={card.card_faces[0].image_uris.small}
+                onClick={(event) => handleClickOnMDFC(event, card)}
+              />
+            );
+          }
+        })
       )}
     </ul>
   );
