86dc49bb9e148013432a323358929d687d46f91c
Adam Crockett <arc1224@gmail.com>
2023-03-30 17:10:01 -0700

    Moved State to parent component

--- src/Components/TierLevelManager.tsx
+++ src/Components/TierLevelManager.tsx
@@ -1,31 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TierLevel from './TierLevel';
 
-const TierLevelManager = () => {
-  const [tierLevels, setTierLevels] = useState<any[]>([]);
-
-  const handleAddTierLevel = () => {
-    setTierLevels([...tierLevels, { tierName: tierLevels.length, cards: [] }]);
-  };
-
-  const handleRemoveTierLevel = (index: number) => {
-    const updatedTierLevels = [...tierLevels];
-    updatedTierLevels.splice(index, 1);
-    setTierLevels(updatedTierLevels);
-  };
-
-  const handleEditTierLevel = (index: number, newName: string) => {
-    const updatedTierLevels = [...tierLevels];
-    updatedTierLevels[index].tierName = newName;
-    setTierLevels(updatedTierLevels);
-  };
+const TierLevelManager = ({
+  tierCards,
+  tierLevels,
+  handleAddTierLevel,
+  handleRemoveTierLevel,
+  handleEditTierLevel
+}: any) => {
   return (
     <div>
       <div>
         <button onClick={handleAddTierLevel}>Add Tier</button>
       </div>
       <ul>
-        {tierLevels.map((tierLevel, index) => {
+        {tierLevels.map((tierLevel: any, index: number) => {
           return (
             <TierLevel
               key={index}
