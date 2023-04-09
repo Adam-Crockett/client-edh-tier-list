df1a97c43c310a5f980e182e6a7ea66fabef6038
Adam Crockett <arc1224@gmail.com>
2023-04-09 09:25:16 -0700

    refactor set fetch

--- src/customHooks/useGetSets.tsx
+++ src/customHooks/useGetSets.tsx
@@ -10,23 +10,21 @@ export default function useGetSets() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        axios.get(`${process.env.REACT_APP_API_URL}/sets`).then(({ data }) => {
-          setResData(() => {
-            if (Array.isArray(data)) {
-              data.sort((a: SetData, b: SetData) => {
-                return a.releaseDate > b.releaseDate ? -1 : 1;
-              });
-              setLoading(false);
-              return data;
-              console.log('sorted data', data);
-            } else {
-              console.log('data is not an array', data);
-              return data;
-            }
+        const { data } = await axios.get(
+          `${process.env.REACT_APP_API_URL}/sets`
+        );
+        if (Array.isArray(data)) {
+          data.sort((a: SetData, b: SetData) => {
+            return a.releaseDate > b.releaseDate ? -1 : 1;
           });
-        });
+          setResData(data);
+        } else {
+          setError('Error fetching data');
+        }
       } catch (e) {
         setError('Error fetching data');
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
